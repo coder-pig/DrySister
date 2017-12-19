139a188e21fc86a9488afe4febfe5e29ffd0c988
zpj <779878443@qq.com>
2017-12-19 22:56:54 +0800

    fix code in as3.0

--- app/src/main/java/com/coderpig/drysisters/network/SisterApi.java
+++ app/src/main/java/com/coderpig/drysisters/network/SisterApi.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
  */
 public class SisterApi {
     private static final String TAG = "Network";
-    private static final String BASE_URL = "http://gank.io/api/data/福利/";
+    private static final String BASE_URL = "http://gank.io/api/data/%e7%a6%8f%e5%88%a9/";
 
     /**
      * 查询妹子信息
